70921618a75ccae967516293f0d1f3205a9dff09
haowei0315 <476251935@163.com>
2017-09-08 13:50:25 +0800

    初始化提交

---
+++ ky.common/src/main/java/anxin/ky/common/App.java
@@ -0,0 +1,30 @@
+package anxin.ky.common;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );//ertyert
+    }
+    
+    private void aa() {
+		// TODO Auto-generated method stub
+
+	}
+    
+    private void aaasd() {
+		//f TODO Auto-generated method stub
+	    //ertert
+//而特他
+	}
+    
+    
+    private void asdadsasda() {
+		// TODO Auto-generated method stub
+
+	}
+}
---
+++ ky.common/src/main/java/anxin/ky/dao/BaseDao.java
@@ -0,0 +1,25 @@
+package anxin.ky.dao;
+
+public class BaseDao {
+	
+	private void tewet() {
+		// TODO Auto-generated method stub
+               //eqwwwwwwwwwwwwwwwwww
+	}
+	
+	private void create() {
+		System.err.println("create...");
+//vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv0908
+	}
+	
+	private void delete() {
+		// TODO Auto-generated method stub
+
+	}
+	
+	private void add() {
+		// TODO Auto-generated method stub
+		System.err.println(11111111);
+	}
+
+}
